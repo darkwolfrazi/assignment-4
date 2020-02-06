9975b66fe883136101a60c465d776a81974766d2
Razi Montgomery <darkwolfrazi@yahoo.com>
2020-02-05 19:50:34 -0800

    work on and completed question 1.

--- script.js
+++ script.js
@@ -3,7 +3,22 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
-
+const min = function(n1, n2) {
+    if (n1 < n2){
+    return n1
+    }
+    else if (n2 < n1)
+    {
+    return n1
+    }
+    else (n1 = n2)
+    {
+        return "they are both equal";
+    }
+}
+console.log(min(5,10));
+console.log(min(-32,-6));
+console.log(min(0,0));
 
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
@@ -14,12 +29,14 @@ let p = 3.14;
 let radius = (5);
 
 const calculateCircumference = function (n){
-    return u * p * (x);
+    return n * p*u;
 }
 
-console.log(u*p*radius);
+console.log(calculateCircumference(5));
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
+
+
